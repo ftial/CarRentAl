4cc4cc783b657b9a742b2276ac20dd5c7e5b6cfd
unknown <alfaplatform@gmail.com>
2023-12-15 20:50:20 +0100

    Second commit

---
+++ requires/header.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand ms-3 me-5" href="#">AutoRentAl</a>
+    <div class="row">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item position-relative">
+                <a class="nav-link" href="#">Home</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Auto Listings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Testimonials</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <div class="container justify-content-end">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <button type="button" class="btn btn-outline-success me-2">Log in</button>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-outline-secondary">Sign up</button>
+            </li>
+        </ul>
+    </div>
+</nav>
