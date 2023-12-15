3c907dd122181afd098aed1f840a8466b7b5cda1
unknown <alfaplatform@gmail.com>
2023-12-15 20:23:36 +0100

    firstCommit

---
+++ Views/home.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+    <body>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand ms-3" href="#">AutoRentAl</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item position-relative">
+                                <a class="nav-link" href="#">Home</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Car Listings</a>
+                            </li>
+                        </ul>
+                    <div class="container justify-content-end">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <button type="button" class="btn btn-outline-success me-2">Log in</button>
+                            </li>
+                            <li class="nav-item">
+                                <button type="button" class="btn btn-outline-secondary">Sign up</button>
+                            </li>
+                        </ul>
+                    </div>
+            </nav>
+    </body>
+</html>
